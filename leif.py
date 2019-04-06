48fbef6c9bdd842fc4dc21da825b5b1e9c23885d
Your Name <you@example.com>
2019-04-06 14:50:37 +0800

    two

--- leif.py
+++ leif.py
@@ -2,6 +2,7 @@
 keylist=[]
 valuelist=[]
 keyrepeat=[]
+key_val_repeat=[]
 with open("1.txt","r+") as f:
     line=f.readline()
     while line.strip():
@@ -11,10 +12,17 @@ with open("1.txt","r+") as f:
             keyrepeat.append(key)
             if val in valuelist:
                 print ("\033[1;31;40mkey and value repeat:%s=%s\033[0m" %(key,val))
+                key_val_repeat.append(key)
             else:
-                print("key repeat but value no repeat:%s=%s"%(key,val))
+                print("key repeat but value no repeat:%s"%key)
         else:
             keylist.append(key)
             valuelist.append(val)
         line=f.readline()
-print("key repeat:",keyrepeat)
+with open("1.txt","r+") as f:
+    for line in f:
+        for i in key_val_repeat:
+            if i in line.split("=")[0]:
+                continue
+            else:
+                print line.strip()
