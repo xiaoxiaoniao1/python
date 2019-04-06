eb4e6cfb10ae3f235d3633eaa6a0d8bb6f216921
Your Name <you@example.com>
2019-04-06 16:22:04 +0800

    three

--- leif.py
+++ leif.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python 
+#!_*_coding:utf-8_*_
 keylist=[]
 valuelist=[]
 keyrepeat=[]
@@ -12,6 +13,7 @@ with open("1.txt","r+") as f:
             keyrepeat.append(key)
             if val in valuelist:
                 print ("\033[1;31;40mkey and value repeat:%s=%s\033[0m" %(key,val))
+                key_val=key+"="+val
                 key_val_repeat.append(key)
             else:
                 print("key repeat but value no repeat:%s"%key)
@@ -19,10 +21,11 @@ with open("1.txt","r+") as f:
             keylist.append(key)
             valuelist.append(val)
         line=f.readline()
+print("去重后的结果")
 with open("1.txt","r+") as f:
     for line in f:
         for i in key_val_repeat:
-            if i in line.split("=")[0]:
+            if i in line.split("=")[0] :
                 continue
             else:
                 print line.strip()
