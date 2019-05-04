b931eb48629580e976f288dc7538fb3cb9906090
xiaoxiaoniao1 <49300445+xiaoxiaoniao1@users.noreply.github.com>
2019-05-04 15:23:06 +0800

    Update base.py

--- angus/crawl_inherit/model/base.py
+++ angus/crawl_inherit/model/base.py
@@ -1,17 +1,23 @@
 import mysql.connector
 
+#db = mysql.connector.connect(host="localhost", user="root", passwd="root", database="crawel")
+db = None
+
 class Base:
-    db = None
-
+    # use singleton pattern to initize db
     def __init__(self):
-        pass
-
-    def getdb(self):
-        if not self.db:
-            self.db = mysql.connector.connect(
+        global db
+        if not db:
+            db = mysql.connector.connect(
               host="localhost",
               user="root",
               passwd="root",
               database="crawel"
             )
-        return self.db
+        return db
+
+    def getdb(self):
+        # can not call self.__init__() here, because it would be called by Url
+        #self.__init__()
+        global db
+        return db
